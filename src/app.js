de46242ed6d54f4dec2101a514769202e92ce7fb
Amir Hossein Kargar <amirkaargar@hotmail.com>
2024-08-22 15:41:16 +0330

    complete home page

---
+++ src/app.js
@@ -0,0 +1,36 @@
+let nav = document.querySelector(".header .navbar");
+
+document.querySelector('#menu').addEventListener('click', () => {
+    nav.classList.add('active')
+})
+document.querySelector('#close').addEventListener('click', () => {
+    nav.classList.remove('active')
+})
+
+// ? mouse move
+document.addEventListener("mousemove", move);
+
+function move(e) {
+  this.querySelectorAll(".move").forEach((layer) => {
+    const speed = layer.getAttribute("data-speed");
+    const x = (window.innerWidth - e.pageX * speed) / 120;
+    const y = (window.innerWidth - e.pageY * speed) / 120;
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
+};
+
+// ? Animation
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 2, y: 10 });
+gsap.from(".navbar .nav_item", {
+  opacity: 0,
+  duration: 1,
+  delay: 2 - 1,
+  y: 30,
+  stagger: 0.2,
+});
+
+gsap.from(".title", { opacity: 0, duration: 1, delay: 1.6, y: 30 });
+gsap.from(".description", { opacity: 0, duration: 1, delay: 1.8, y: 30 });
+gsap.from(".btn", { opacity: 0, duration: 1, delay: 2.1, y: 30 });
+gsap.from(".image", { opacity: 0, duration: 1, delay: 2.6, y: 30 });
