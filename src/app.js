5d6db4a41273306d4dc86981260f20ce8b0cfb70
Amir Hossein Kargar <amirkaargar@hotmail.com>
2024-08-24 17:13:51 +0330

    menu section added

--- src/app.js
+++ src/app.js
@@ -34,3 +34,4 @@ gsap.from(".title", { opacity: 0, duration: 1, delay: 1.6, y: 30 });
 gsap.from(".description", { opacity: 0, duration: 1, delay: 1.8, y: 30 });
 gsap.from(".btn", { opacity: 0, duration: 1, delay: 2.1, y: 30 });
 gsap.from(".image", { opacity: 0, duration: 1, delay: 2.6, y: 30 });
+
