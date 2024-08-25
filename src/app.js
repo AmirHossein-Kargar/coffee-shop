07b9875c597f4d4e7a48fa4c25d640d826cb9fe2
Amir Hossein Kargar <amirkaargar@hotmail.com>
2024-08-25 17:55:23 +0330

    complete menu section

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
+import { productsData } from "./products.js";
+const menu = document.querySelector(".menu");
+
+// ? GET DATA
+class Products {
+  getProduct() {
+    return productsData;
+  }
+}
+// ? SHOW DATA
+class UI {
+  displayProducts(products) {
+    let result = "";
+    products.forEach((item) => {
+      result +=`<div class="card" id="card">
+        <div class="card__image">
+          <img src=${item.imageUrl} alt="">
+        </div>
+        <p class="card__title">${item.title}</p>
+        <p class="card__price">${item.price}$</p>
+        <p class="card__description">${item.desc}</p>
+        <a href="#" class="btn card__btn" data-id=${item.id}>ORDER NOW</a>
+       </div>`;
+       menu.innerHTML = result
+    });
+  }
+}
+// ? LOCAL STORAGE
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const products = new Products();
+  const productsData = products.getProduct();
+  // console.log(productsData);
+  const ui = new UI()
+  ui.displayProducts(productsData)
+});
+
 let nav = document.querySelector(".header .navbar");
 
-document.querySelector('#menu').addEventListener('click', () => {
-    nav.classList.add('active')
-})
-document.querySelector('#close').addEventListener('click', () => {
-    nav.classList.remove('active')
-})
+document.querySelector("#menu").addEventListener("click", () => {
+  nav.classList.add("active");
+});
+document.querySelector("#close").addEventListener("click", () => {
+  nav.classList.remove("active");
+});
 
 // ? mouse move
 document.addEventListener("mousemove", move);
@@ -18,7 +56,7 @@ function move(e) {
 
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
   });
-};
+}
 
 // ? Animation
 gsap.from(".logo", { opacity: 0, duration: 1, delay: 2, y: 10 });
@@ -35,3 +73,17 @@ gsap.from(".description", { opacity: 0, duration: 1, delay: 1.8, y: 30 });
 gsap.from(".btn", { opacity: 0, duration: 1, delay: 2.1, y: 30 });
 gsap.from(".image", { opacity: 0, duration: 1, delay: 2.6, y: 30 });
 
+// ? Logic for changing background color when we scroll !
+const myNav = document.querySelector(".header");
+window.onscroll = () => {
+  if (
+    document.body.scrollTop >= 200 ||
+    document.documentElement.scrollTop >= 200
+  ) {
+    myNav.classList.add("nav-colored");
+    myNav.classList.remove("nav-transparent");
+  } else {
+    myNav.classList.add("nav-transparent");
+    myNav.classList.remove("nav-colored");
+  }
+};
---
+++ src/products.js
@@ -0,0 +1,58 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Espresso Coffee",
+    price: 25,
+    desc: "made with specially roasted beans and an espresso machine.",
+    imageUrl: "./images/Espresso.jpeg",
+  },
+  {
+    id: 2,
+    title: "Iced Americano",
+    price: 30,
+    desc: "combining espresso shots with cold water and ice",
+    imageUrl: "./images/iced-americano.jpg",
+  },
+  {
+    id: 3,
+    title: "Caffè Americano",
+    price: 30,
+    desc: "combining espresso shots with hot water",
+    imageUrl: "./images/amerciano.jpeg",
+  },
+  {
+    id: 4,
+    title: "Cappuccino",
+    price: 35,
+    desc: "Espresso-based coffee drink that is traditionally prepared with steamed milk",
+    imageUrl: "./images/Cappuccino.jpeg",
+  },
+  {
+    id: 5,
+    title: "Espresso Con Panna",
+    price: 35,
+    desc: "single or double shot of espresso topped with whipped cream",
+    imageUrl: "./images/EspressoConPanna.jpeg",
+  },
+  {
+    id: 6,
+    title: "Flat White",
+    price: 40,
+    desc: "a blend of micro-foamed milk poured over a single or double shot of espresso",
+    imageUrl: "./images/FlatWhite.jpeg",
+  },
+  {
+    id: 7,
+    title: "Caffè Latte",
+    price: 40,
+    desc: "made up of one or two shots of espresso, steamed milk and a final, thin layer of frothed milk on top",
+    imageUrl: "./images/CaffèLatte.jpeg",
+  },
+  {
+    id: 8,
+    title: "Caffè Mocha",
+    price: 45,
+    desc: "espresso, bittersweet mocha sauce and steamed milk",
+    imageUrl: "./images/CaffèMocha.jpeg",
+  },
+];
